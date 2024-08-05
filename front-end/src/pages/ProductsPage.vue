ea8c701614dc82097cfb1de231717e3432773de0
roseharris <59656052+roseharris@users.noreply.github.com>
2024-08-05 16:17:33 -0700

    Pick back up on 01_12_b

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,28 +1,26 @@
 <template>
   <h1>Products</h1>
   <div class="grid-wrap">
-    <div
-      class="product-item"
-      v-for="product in products"
-      :key="product.id"
-    >
+    <div class="product-item" v-for="product in products" :key="product.id">
       <img :src="product.imageName" />
       <h3 class="product-name">{{ product.name }}</h3>
       <p class="product-price">{{ product.price }}</p>
-      <button>View Details</button>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
-import { products } from '../temp-data';
+import { products } from "../temp-data";
 
 export default {
   name: "ProductsPage",
   data() {
     return {
       products,
-    }
-  }
-}
+    };
+  },
+};
 </script>
